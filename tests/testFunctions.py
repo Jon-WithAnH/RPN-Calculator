6e5c05c9cf9398147d946350d136fc94ca936d11
RandomProgrammer1124 <averagehero2@gmail.com>
2021-03-10 20:14:42 -0500

    Add files via upload
    Expanded test cases.
    Now includes adding and subtraction

--- tests/testFunctions.py
+++ tests/testFunctions.py
@@ -5,35 +5,46 @@ class testCases():
 
     def __init__(self):
         self._testObj = engine.engine()
-        self.testAppend()
+        self.append()
         self.peek()
         self.pop()
-    def testAppend(self):
+        self.add()
+        self.subtraction()
+
+    def append(self):
         correctList = [0,1,2,3,4,5,6,7,8,9]
         for x in range(10):
             self._testObj.push(x)
         assert self._testObj.getWholeStructure() == correctList, "Error appending to structure"
 
     def peek(self):
-        assert self._testObj.peek() == 9, f"Error in peek: {self._testObj.getWholeStructure()[len(self._testObj.getWholeStructure())-1]} does not equal 9"
-        assert self._testObj.peek() == 9, f"Removal happened in peek: {self._testObj.getWholeStructure()[len(self._testObj.getWholeStructure()) - 1]} does not equal 9"
+        assert self._testObj.peek() == 9, f"Error in peek: " \
+                                          f"{self._testObj.getWholeStructure()[len(self._testObj.getWholeStructure())-1]} does not equal 9"
+        assert self._testObj.peek() == 9, f"Removal happened in peek: " \
+                                          f"{self._testObj.getWholeStructure()[len(self._testObj.getWholeStructure()) - 1]} does not equal 9"
 
     def pop(self):
         beforePop = len(self._testObj.getWholeStructure())
         self._testObj.pop()
         afterPop = len(self._testObj.getWholeStructure())
-        assert afterPop == beforePop-1, f"\nError in func pop call: \nLengths should be the same. {beforePop-1} != {afterPop}"
+        assert afterPop == beforePop-1, f"\nError in func pop call: \n" \
+                                        f"Lengths should be the same. {beforePop-1} != {afterPop}"
 
-    def testAdd(self):
+    def add(self):
+        # operator assumed "+"
         self._testObj.push(1)
         self._testObj.push(2)
-        # operator assumed "+"
+        result = MathOperations.add(self._testObj) # 1+2
+        assert result == 3, f"Add test case failed\n" \
+                            f"{result} != 3"
 
-        #assert MathOperations.operations.add(self._testObj) == 3, "Add test case failed"
+    def subtraction(self):
+        self._testObj.push(1)
+        self._testObj.push(2)
+        result = MathOperations.subtraction(self._testObj) # 2-1
+        assert result == 1, f"Add test case failed\n" \
+                            f"{result} != 1"
 
-
-    def testSubtraction(self):
-        pass
 
 
 testCases()
